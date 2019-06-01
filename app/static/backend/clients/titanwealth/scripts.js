c5f45c2c14ad2ab2d0fd798908b792f0dde35696
John Lopez <johnlopez@Johns-MacBook-Pro.local>
2019-06-01 12:24:27 -0500

    updates

--- app/static/backend/clients/titanwealth/scripts.js
+++ app/static/backend/clients/titanwealth/scripts.js
@@ -4,7 +4,7 @@ $(document).ready(function() {
   $('form').parsley();
   $( "#assignably_form" ).submit(function( event ) {
     event.preventDefault();
-    swal("<div class=\"spinner-border text-success\" role=\"status\"><span class=\"sr-only\">Loading...</span></div>")
+    swal("Sending your deal...", "info");
     $.ajax({
       url: 'https://assignably.herokuapp.com/api/deals',
       async:true,
